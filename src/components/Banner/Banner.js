5bd358c740a3479b0ffd18529b7867bab3a6d243
brayannfave <brayann.fave@gmail.com>
2021-12-23 01:06:31 -0300

    probando token

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -5,6 +5,7 @@ export const Banner = () => {
     return (
         <div className="banner">
             <h1>Bienvenidos a CerveMark</h1>
+            <a>revisa nuestras ofertas</a>
         </div>
     )
 }
